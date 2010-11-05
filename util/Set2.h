eb7727f5c27f4758e59277fc17adc085b8a371fe
Ryan Pavlik <rpavlik@iastate.edu>
2010-11-05 11:15:31 -0500

    Documentation

--- util/Set2.h
+++ util/Set2.h
@@ -1,5 +1,6 @@
 /** @file Set2.h
-	@brief
+	@brief Template class for holding two interchangeable elements that
+	we nevertheless want to be compatible with STL containers and algos
 
 	@date 2010
 
@@ -20,6 +21,8 @@ namespace util {
 
 template<typename T>
 class Set2 {
+	/// @todo How to enforce a single representation for cases in which
+	/// ! (a < b) but also ! (b < a), but a != b in some meaningful way?
 	public:
 		Set2(T const& a, T const& b) :
 				_first(b < a ? b : a),
@@ -27,7 +30,8 @@ class Set2 {
 		Set2(Set2<T> const& other) :
 			_first(other._first),
 			_second(other._second) {}
-		
+
+		/// Assignment operator
 		Set2<T> const& operator=(Set2<T> const& other) {
 			if (&other == this) {
 				/// Shortcut self-assignment
@@ -37,21 +41,29 @@ class Set2 {
 			_second = other._second;
 			return *this;
 		}
-		
+
+		/// Mutator: must change both at once to enforce internal order
 		Set2<T> const& set(T const& a, T const& b) {
 			Set2<T> temp(a, b);
 			*this = temp;
 			return *this;
 		}
-		
+
+		/// Accessor for the "smaller" of the two elements
 		T const& first() const { return _first; }
+
+		/// Accessor for the "larger" of the two elements
 		T const& second() const { return _second; }
-	
+
 	private:
+		/// Storage of smaller element or element passed first
 		T _first;
+
+		/// Storage of larger element or element passed second
 		T _second;
 };
 
+/// Comparison operator for Set2 containers: uses only operator<
 template<typename T>
 bool operator<(Set2<T> const& a, Set2<T> const& b) {
 	/// The somewhat awkward series of comparisons is to
@@ -62,11 +74,13 @@ bool operator<(Set2<T> const& a, Set2<T> const& b) {
 		return false;
 	} else {
 		return a.second() < b.second();
-	}	
+	}
 }
 
+/// Equality operator for Set2 containers: uses operator==
 template<typename T>
 bool operator==(Set2<T> const& a, Set2<T> const& b) {
+	/// @todo should we be using combined < calls here?
 	return (a.first() == b.first()) && (a.second() == b.second());
 }
 
