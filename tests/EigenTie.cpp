64da510351dea9219c3fa3eff166538589952694
Ryan Pavlik <rpavlik@iastate.edu>
2011-07-27 16:37:58 -0500

    improve tests for tie

--- tests/EigenTie.cpp
+++ tests/EigenTie.cpp
@@ -24,12 +24,31 @@ namespace {
 	void testAssign(double val) {
 		Eigen::Vector3d orig(val, val, val);
 		double x, y, z;
-		util::Tie<3, double>(x, y, z) = orig;
-		BOOST_CHECK_EQUAL(util::Tie(x, y, z), orig);
-	
+		util::TieVector(x, y, z) = orig;
+		BOOST_CHECK_EQUAL(util::TieVector(x, y, z), orig);
+
 		BOOST_CHECK_EQUAL(x, val);
 		BOOST_CHECK_EQUAL(y, val);
 		BOOST_CHECK_EQUAL(z, val);
+
+		x++;
+		BOOST_CHECK_NE(util::TieVector(x, y, z), orig);
+
+	}
+	void testTieTieAssign(double val) {
+		Eigen::Vector3d orig(val, val, val);
+		double x, y, z;
+		double a, b, c;
+		util::TieVector(x, y, z) = orig;
+		util::TieVector(a, b, c) = util::TieVector(x, y, z);
+		BOOST_CHECK_EQUAL(util::TieVector(x, y, z), util::TieVector(a, b, c));
+
+		BOOST_CHECK_EQUAL(a, val);
+		BOOST_CHECK_EQUAL(b, val);
+		BOOST_CHECK_EQUAL(c, val);
+
+		x++;
+		BOOST_CHECK_NE(util::TieVector(x, y, z), util::TieVector(a, b, c));
 	}
 }
 
@@ -38,6 +57,14 @@ BOOST_AUTO_TEST_CASE(ThreeZerosAssign) {
 }
 
 BOOST_AUTO_TEST_CASE(ThreeOnesAssign) {
+	testAssign(1.0);
+}
+
+BOOST_AUTO_TEST_CASE(ThreeZerosTieTieAssign) {
+	testAssign(0.0);
+}
+
+BOOST_AUTO_TEST_CASE(ThreeOnesTieTieAssign) {
 	testAssign(1.0);
 }
 
