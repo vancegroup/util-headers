8472f9b33da01debc27414cd88ef607a91804187
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-09 10:00:49 -0600

    Start implementing search path element

--- util/SearchPath.h
+++ util/SearchPath.h
@@ -27,6 +27,69 @@
 // - none
 
 // Standard includes
-// - none
+#include <string>
+
+namespace util {
+
+/// @addtogroup DataStructures Data Structures
+/// @{
+
+	namespace detail {
+		namespace {
+			const char PATH_SEPARATOR = ';';
+			const char PLACEHOLDER = '?';
+		} // end of anonymous namespace
+	} // end of namespace detail
+
+	/** @brief Class representing an element in a search path.
+
+		This uses the generalized concept of a search path as used
+		by Lua: a filename with a character that is replaced to form
+		a potential filename.
+	*/
+	class SearchPathElement {
+		public:
+			/// @brief default constructor
+			SearchPathElement() {}
+
+			/** @brief Constructor from either a prefix alone or a path with a placeholder
+
+				Three cases:
+					1. /bla/bla/
+					2. /bla/bla/?
+					3. /bla/bla/?.lua
+			*/
+			SearchPathElement(std::string const& elt) {
+				std::size_t placeholder = elt.find(detail::PLACEHOLDER);
+				if (placeholder == std::string::npos) {
+					// Handle case 1
+					// Didn't find a placeholder, so treat the whole thing as the prefix
+					_prefix = elt;
+				} else {
+					// Found a placeholder: split into prefix and suffix
+					// We're in case 2 or 3
+					_prefix = elt.substr(0, placeholder);
+					if (placeholder + 1 < elt.size()) {
+						// We actually have a suffix: case 3
+						_suffix = elt.substr(placeholder + 1);
+					}
+				}
+			}
+
+			/** @brief Constructor from a prefix and suffix
+			*/
+			SearchPathElement(std::string const & pre, std::string const & suff)
+				: _prefix(pre)
+				, _suffix(suff)
+			{}
+		private:
+			std::string _prefix;
+			std::string _suffix;
+	};
+
+
+/// @}
+
+} // end of namespace util
 
 #endif // INCLUDED_SearchPath_h_GUID_7a79b983_9d8b_4185_80ab_77146a676bdf
