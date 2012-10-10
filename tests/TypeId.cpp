e09f458b00a78ddf4c151bcb8f23fe685fddc504
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-10 16:54:11 -0500

    Add more tests

--- tests/TypeId.cpp
+++ tests/TypeId.cpp
@@ -25,6 +25,13 @@ using util::TypeId;
 typedef std::vector<util::TypeId> TypeIdVector;
 typedef std::set<util::TypeId> TypeIdSet;
 
+struct Fixture {
+	Fixture() : StringId(typeid(std::string)), IntId(typeid(int)) {}
+	TypeId StringId;
+	TypeId IntId;
+	TypeId EmptyId;
+};
+
 BOOST_AUTO_TEST_CASE(DefaultConstructor) {
 	BOOST_REQUIRE_NO_THROW(TypeId());
 }
@@ -34,17 +41,39 @@ BOOST_AUTO_TEST_CASE(TypeinfoConstructor) {
 	BOOST_REQUIRE_NO_THROW(TypeId(typeid(std::string)));
 }
 
+BOOST_AUTO_TEST_CASE(FixtureConstruction) {
+	BOOST_REQUIRE_NO_THROW(Fixture());
+}
+
 BOOST_AUTO_TEST_CASE(EqualityComparison) {
 	BOOST_CHECK_EQUAL(TypeId(), TypeId());
 	BOOST_CHECK_EQUAL(TypeId(typeid(int)), TypeId(typeid(int)));
 	BOOST_CHECK_EQUAL(TypeId(typeid(std::string)), TypeId(typeid(std::string)));
 
 	BOOST_CHECK_NE(TypeId(), TypeId(typeid(std::string)));
+	BOOST_CHECK_NE(TypeId(typeid(int)), TypeId(typeid(std::string)));
 
 	BOOST_CHECK(TypeId(typeid(std::string)) == typeid(std::string));
 	BOOST_CHECK(typeid(std::string) == TypeId(typeid(std::string)));
 
 	BOOST_CHECK(TypeId() != typeid(std::string));
 	BOOST_CHECK(typeid(std::string) != TypeId());
+}
+
+BOOST_AUTO_TEST_CASE(Ordering) {
+	Fixture f;
+	BOOST_CHECK(!(f.EmptyId < f.EmptyId));
+	BOOST_CHECK(!(f.EmptyId < f.IntId));
+	BOOST_CHECK(!(f.EmptyId < typeid(int)));
+	BOOST_CHECK(!(f.EmptyId < f.StringId));
+	BOOST_CHECK(!(f.EmptyId < typeid(std::string)));
+
+	BOOST_CHECK(f.StringId < f.EmptyId);
+	BOOST_CHECK(typeid(std::string) < f.EmptyId);
+	BOOST_CHECK(f.IntId < f.EmptyId);
+	BOOST_CHECK(typeid(int) < f.EmptyId);
+
+	BOOST_CHECK((f.IntId < f.StringId) || (f.StringId < f.IntId));
+	BOOST_CHECK(!((f.IntId < f.StringId) && (f.StringId < f.IntId)));
 }
 
