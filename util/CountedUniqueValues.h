be57234cd091ad2df8f3b53e5409886e4d8fd112
Ryan Pavlik <rpavlik@iastate.edu>
2014-05-12 14:09:44 -0500

    Bump copyright.

--- util/CountedUniqueValues.h
+++ util/CountedUniqueValues.h
@@ -3,7 +3,7 @@
 
 	@versioninfo@
 
-	@date 2010
+	@date 2010, 2014
 
 	@author
 	Ryan Pavlik
@@ -13,7 +13,7 @@
 	Human-Computer Interaction Graduate Program
 */
 
-//          Copyright Iowa State University 2010.
+//          Copyright Iowa State University 2010, 2014.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
